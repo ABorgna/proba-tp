93c9df6bf9c590534d263277965330df6eb56186
Agustin Borgna <hola@aborgna.com.ar>
2015-11-15 16:41:37 -0300

    Parte teorica del primero

---
+++ calculos.hs
@@ -0,0 +1,16 @@
+
+ecm_mv n theta = n * theta / (n+1)
+ecm_m n theta = theta^2 / (3*n)
+
+thetas = [1,10,40]
+ns = [5,30,60]
+
+cuadrito fn ns ts = mapM_ (\n -> putStrLn $ show $ map (fn n) ts) ns
+
+main = do
+    putStrLn "Cuadrito ECM_mv"
+    cuadrito ecm_mv ns thetas
+    putStrLn ""
+    putStrLn "Cuadrito ECM_m"
+    cuadrito ecm_m ns thetas
+
